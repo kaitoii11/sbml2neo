942593b5012e908714a9c751b5572c762633aa4c
kaitoii11 <kaitoii1111@gmail.com>
2018-05-30 23:46:12 +0900

    added model id

--- code/modelparser.py
+++ code/modelparser.py
@@ -5,7 +5,8 @@ from libsbml import *
 
 class ModelParser:
     def __init__(self,model=None):
-        self.model = readSBML(model).getModel()
+        self.model = readSBMLFromFile(model).getModel()
+        self.model_id = self.model.getId()
         self.reactions = self.model.getListOfReactions()
         self.species = self.model.getListOfSpecies()
 
